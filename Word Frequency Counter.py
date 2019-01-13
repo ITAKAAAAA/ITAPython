373cb0bfb8593f71758f757b567bbd94a299f383
ITAKAAAAA <maxste000@gmail.com>
2019-01-13 12:21:49 +0100

    Dopo vado avanti giuroo
    Signed-off-by: ITAKAAAAA <maxste000@gmail.com>

--- Word Frequency Counter.py
+++ Word Frequency Counter.py
@@ -1,15 +1,10 @@
-import requests
-from bs4 import BeautifulSoup
-import operator
-
 def contaParole(file):
     word_list = []
     fr = open(file, 'r')
     text = fr.read()
     fr.close
-    words = text.content.lower().split()
+    words = text.lower().split(" ")
     for each_word in words:
         print(each_word)
-        word_list.append(each_word)
 
 contaParole('sample.txt')
